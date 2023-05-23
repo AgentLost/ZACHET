abe7a6799ab844713b5989006f5cea3baafb661d
svisha1 <115243940+svisha1@users.noreply.github.com>
2023-05-23 12:02:36 +0400

    first commit front
    first commit front

---
+++ zachet/src/components/ProductList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+            <div class="product" v-for="product in products">
+            <div><strong>Название:</strong> {{ product.title }}</div>
+            <div><strong>Описание:</strong> {{ product.body }}</div>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        props:{
+            products:{
+                type:Array,
+                required: true
+            }
+        }
+    }
+</script>
+<style scoped>
+.product{
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+</style>
